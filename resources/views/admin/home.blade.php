2e46ba5f2cfcf1d79de591c90066e20fdd7655d1
MikePio <michelepiopilla@gmail.com>
2023-06-17 17:50:16 +0200

    Completed the design part for guest and admin home

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,9 +2,23 @@
 
 @section('content')
 
-<div class="container py-5">
-    <h1>Home admin</h1>
-    <h1>You are logged in!</h1>
+<div class="container p-5 text-center">
+    {{-- <h1>Home admin</h1> --}}
+    <h1 class="pb-3">Welcome!</h1>
+    <p class="fs-5 text">
+    Welcome to the post management dashboard! Our home page offers you an intuitive and powerful interface to manage your content. With our dashboard, you can easily create, edit and delete your posts quickly and efficiently.
+    <br>
+    <br>
+    Managing posts is easy with editing and deleting options available right from the home page. With just a few clicks, you can make changes to content, update information, or remove posts that are no longer relevant.
+    <br>
+    <br>
+    In addition, our dashboard offers you the convenience of being able to "add" new posts right from the home page. Thanks to a dedicated button, you can easily create new content and publish it in just a few steps. Just click "New Post" and you'll be guided through an intuitive interface to enter the title, post content, and other pertinent information.
+    <br>
+    <br>
+    We're sure you'll find our post management home page intuitive and easy to use.
+    <br>
+    Explore its features and enjoy a hassle-free content management experience!
+    </p>
 </div>
 
 @endsection
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.app')
 @section('content')
-
-<div class="container py-5">
-        <h1>Home guest</h1>
-        <h1>Welcome guest</h1>
+    <div class="container p-5 text-center">
+        {{-- <h1>Home guest</h1> --}}
+        <h1>Welcome!</h1>
+        <h2>Here you can manage your dashboard</h2>
+        <h2>Register or log in to access as admin</h2>
+        <a class="btn btn-primary m-3 text-white" href="{{ route('login') }}">{{ __('Login') }}</a>
+        <a class="btn btn-primary m-3 text-white" href="{{ route('register') }}">{{ __('Register') }}</a>
     </div>
-
 @endsection
