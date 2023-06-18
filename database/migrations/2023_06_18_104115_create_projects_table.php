ff7e77ff588b8ecd5c18204ac3d629d25dc3f503
MikePio <michelepiopilla@gmail.com>
2023-06-18 16:18:29 +0200

    Completed migration

---
+++ database/migrations/2023_06_18_104115_create_projects_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->string('image');
+            $table->string('category');
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->string('url')->nullable();
+            $table->string('produced_for')->nullable();
+            $table->text('collaborators')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
