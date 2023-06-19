e1cf2e3d18fb2546cf6d2593569cedcffe2b8b03
MikePio <michelepiopilla@gmail.com>
2023-06-19 19:41:59 +0200

    Added some improvements regarding error styling

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -35,49 +35,49 @@
     @enderror
     <div class="mb-3">
         <label for="category" class="form-label">Category</label>
-        <input type="text" class="form-control" id="category" name="category" placeholder="Category" value="{{ old('category')}}">
+        <input type="text" class="form-control @error('category') is-invalid @enderror" id="category" name="category" placeholder="Category" value="{{ old('category')}}">
     </div>
     @error('category')
       <p class="text-danger">{{ $message }}</p>
     @enderror
     <div class="mb-3">
         <label for="start_date" class="form-label">Start date</label>
-        <input type="text" class="form-control" id="start_date" name="start_date" placeholder="2023-12-31" value="{{ old('start_date')}}">
+        <input type="text" class="form-control @error('start_date') is-invalid @enderror" id="start_date" name="start_date" placeholder="YYYY-MM-DD" value="{{ old('start_date')}}">
     </div>
     @error('start_date')
     <p class="text-danger">{{ $message }}</p>
   @enderror
     <div class="mb-3">
         <label for="end_date" class="form-label">End date</label>
-        <input type="text" class="form-control" id="end_date" name="end_date" placeholder="2023-12-31" value="{{ old('end_date')}}">
+        <input type="text" class="form-control @error('end_date') is-invalid @enderror" id="end_date" name="end_date" placeholder="YYYY-MM-DD" value="{{ old('end_date')}}">
     </div>
     @error('end_date')
     <p class="text-danger">{{ $message }}</p>
   @enderror
     <div class="mb-3">
         <label for="url" class="form-label">url</label>
-        <input type="text" class="form-control" id="url" name="url" placeholder="url" value="{{ old('url')}}">
+        <input type="text" class="form-control @error('url') is-invalid @enderror" id="url" name="url" placeholder="url" value="{{ old('url')}}">
     </div>
     @error('url')
     <p class="text-danger">{{ $message }}</p>
   @enderror
     <div class="mb-3">
         <label for="produced_for" class="form-label">Produced for</label>
-        <input type="text" class="form-control" id="produced_for" name="produced_for" placeholder="personal use, name client" value="{{ old('produced_for')}}">
+        <input type="text" class="form-control @error('produced_for') is-invalid @enderror" id="produced_for" name="produced_for" placeholder="personal use, name client" value="{{ old('produced_for')}}">
     </div>
     @error('produced_for')
     <p class="text-danger">{{ $message }}</p>
   @enderror
     <div class="mb-3">
         <label for="collaborators" class="form-label">Collaborators</label>
-        <input type="text" class="form-control" id="collaborators" name="collaborators" placeholder="Collaborators" value="{{ old('collaborators')}}">
+        <input type="text" class="form-control @error('collaborators') is-invalid @enderror" id="collaborators" name="collaborators" placeholder="Collaborators" value="{{ old('collaborators')}}">
     </div>
     @error('collaborators')
     <p class="text-danger">{{ $message }}</p>
   @enderror
     <div class="mb-3">
         <label for="description" class="form-label">Description</label>
-        <textarea type="text" class="form-control" id="description" name="description" placeholder="Description" style="height: 200px;">{{ old('description')}}</textarea>
+        <textarea type="text" class="form-control @error('description') is-invalid @enderror" id="description" name="description" placeholder="Description" style="height: 200px;">{{ old('description')}}</textarea>
     </div>
 
     <button type="submit" class="btn btn-primary mt-3">Submit</button>
