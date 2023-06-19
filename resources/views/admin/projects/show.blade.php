6b801c909e0e5c175184a81ebe1d2da9ef0e83cb
MikePio <michelepiopilla@gmail.com>
2023-06-20 01:31:55 +0200

    Added image in show.blade.php

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -19,7 +19,7 @@
     </form>
   </h1>
 
-  {{-- <img src="{{ $project->image }}" class="py-2"> --}}
+  <img src="{{ asset('storage/' . $project->image_path) }}" class="py-2 w-25" alt="{{ $project->name }}">
   <h5 class="py-2"><strong class="text-decoration-underline">Id:</strong> {{ $project->id }}</h5>
   <h5 class="py-2"><strong class="text-decoration-underline">Slug:</strong> {{ $project->slug }}</h5>
   <p class="py-2"><strong class="text-decoration-underline">Description:</strong> {!! $project->description !!}</p>
