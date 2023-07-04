b6c29404b70dcbff43575eb7f4606438d8818f9b
MikePio <michelepiopilla@gmail.com>
2023-07-04 13:14:29 +0200

    Created dashboard view showing the number of posts

--- routes/web.php
+++ routes/web.php
@@ -27,12 +27,13 @@ Route::middleware(['auth', 'verified'])
     ->prefix('admin')
     ->group(function(){
         Route::get('/', [DashboardController::class, 'index'])->name('home');
+        Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
         Route::resource('projects', ProjectController::class);
     });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
